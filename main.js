700501ff66dde71db41f6009ef390a98696638ee
Vincent Dupain <vdupain@gmail.com>
2014-10-08 13:27:33 +0200

    error reading file

--- main.js
+++ main.js
@@ -1,39 +1,45 @@
 var mongoose = require('mongoose');
 var mongodbUri = process.env.MONGODB_URI || "mongodb://localhost:27017/";
 mongoose.connect(mongodbUri);
+var carrier = require('carrier');
 var Geonames = require('./app/models/geonames');
-var gstream = require('geonames-stream');
-var through = require('through2');
 var fs = require('fs');
+var inStream = fs.createReadStream('FR.txt', {flags: 'r'});
 
-fs.createReadStream( 'FR.txt' )
-    .pipe( gstream.parser )
-    .pipe( through.obj( function( data, enc, next ){
+carrier.carry(inStream)
+    .on('line', function (line) {
+
+        var fields = line.split('\t');
         var geonames = new Geonames({
-            geonameid: data._id,
-            name: data.name,
-            asciiname: data.asciiname,
-            alternatenames: data.alternatenames,
-            //loc: [fields[4], fields[5]],
-            feature_class: data.feature_class,
-            feature_code: data.feature_code,
-            country_code: data.country_code,
-            alternatenames: data.alternatenames,
-            cc2: data.cc2,
-            admin1: data.admin1_code,
-            admin2: data.admin2_code,
-            admin3: data.admin3_code,
-            admin4: data.admin4_code,
-            population: data.population,
-            elevation: data.elevation,
-            dem: data.dem,
-            timezone: data.timezone,
-            modification_date: data.modification_date
+            geonameid: fields[0],
+            name: fields[1],
+            asciiname: fields[2],
+            alternatenames: fields[3],
+            loc: [fields[4], fields[5]],
+            feature_class: fields[6],
+            feature_code: fields[7],
+            country_code: fields[8],
+            alternatenames: fields[9],
+            cc2: fields[10],
+            admin1: fields[11],
+            admin2: fields[12],
+            admin3: fields[13],
+            admin4: fields[14],
+            population: fields[15],
+            elevation: fields[16],
+            dem: fields[17],
+            timezone: fields[18],
+            modification_date: fields[19]
         });
+        console.log(geonames);
         geonames.save(function (err) {
             if (err)
-                console.log('Error save geonames:', err);
+                console.log('Error save geonames');
         });
-        //console.log(geonames);
-        next();
-    }));
+    }
+)
+    .on('end', function () {
+        console.log('end');
+        process.exit(1);
+    }
+);
