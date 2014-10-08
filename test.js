41f84f9f2e72738e2b2e8919ac3aabc991d747de
Vincent Dupain <vdupain@generali.com>
2014-10-08 13:33:17 +0200

    error reading file

--- test.js
+++
@@ -1,7 +0,0 @@
-var geonames = require('geonames-stream'),
-    fs = require('fs');
-
-fs.createReadStream( 'FR.txt' )
-  .pipe( geonames.parser )
-  .pipe( geonames.stringify )
-  .pipe( process.stdout );
